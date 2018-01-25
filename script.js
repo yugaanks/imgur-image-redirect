e9ad042a349416aeb7f038091d72904a1e3a3ca1
Yugaank Sharma <yugaanks@yahoo.co.in>
2018-01-24 22:03:38 -0500

    fix: redirect urls ending with .jpeg, .gifv and .gif

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const redirectLocation = location.href.slice(0, -4);
+const redirectLocation = location.href.slice(0, location.href.lastIndexOf("."));
 if(location.href!==redirectLocation)
 	location.assign(redirectLocation);
